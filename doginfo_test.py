7bbabe0c9cace1b3c1115a5a212709ed3c80ec76
Bogdan <127588536+korokr@users.noreply.github.com>
2023-03-25 11:03:23 +0200

    Delete doginfo_test.py

--- doginfo_test.py
+++
@@ -1,9 +0,0 @@
-import doginfo as di
-
-sam = di.Dog("Sam", 2.5, "8kg", "White","Jack Russel" )
-print((sam.amount_dogs))
-print(sam.GetFullInfo())
-
-alex = di.Dog("Alex", 6, "26kg", "Brown", "Shar Pei")
-print((alex.amount_dogs))
-print(alex.GetFullInfo())
